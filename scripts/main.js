7252c1f2a5d31674485cb54265206f7d68cc3ef5
Ignacio Romero <nachoromero84@hotmail.com>
2023-11-03 17:02:06 -0300

    added fetch and div creations

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
 var testButton = document.getElementById('testButton');
 
+var accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTlhMDMwNDZjODc1NjBlNWNlNjFlMWE4ZjU2YzkxMCIsInN1YiI6IjY1NDNmZWFmNDFhNTYxMzM2ZDgzMzBhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m-mO8EzxG2yDs8YI7HqkVhUtNylWrgPz1rTT3XF7Fkk"
+var imgEndpoint = "https://image.tmdb.org/t/p/w500";
+var serviceEndpoint = "https://api.themoviedb.org/3/movie/{movie_id}/watch/providers";
+var inputText = document.getElementById("movie-search");
+var modal = document.getElementById('filmInfo');
+
+modal.addEventListener("click",function(e){
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+})
+
+inputText.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      searchMovie(inputText.value);
+    }
+});
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTlhMDMwNDZjODc1NjBlNWNlNjFlMWE4ZjU2YzkxMCIsInN1YiI6IjY1NDNmZWFmNDFhNTYxMzM2ZDgzMzBhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m-mO8EzxG2yDs8YI7HqkVhUtNylWrgPz1rTT3XF7Fkk'
+    }
+};
+  
+
+
 function createFilmOptions(){
     var resultDiv = document.getElementById("resultDiv");
     var newMovie = document.createElement("div");
@@ -8,5 +36,53 @@ function createFilmOptions(){
     resultDiv.appendChild(newMovie);
 }
 
-testButton.addEventListener('click', createFilmOptions);
+function searchMovie(movieName){
+    movieName = movieName.replace(/ /g, '+');
+    console.log(movieName);
+    fetch('https://api.themoviedb.org/3/search/movie?query='+movieName+'&language=es-AR', options)
+    .then(response => response.json())
+    .then(response => listMovies(response.results))
+    .catch(err => console.error(err));
+}
+
+function listMovies(list){
+    var resultDiv = document.getElementById("resultDiv");
+    while (resultDiv.firstChild) {
+        resultDiv.removeChild(resultDiv.firstChild);
+      }
+    list.sort(function(a, b) {
+        return b.popularity - a.popularity;
+    });
+    list.forEach(element => {
+        console.log(element.original_title + " " + element.popularity);
+        var newDiv = document.createElement("div");
+
+        newDiv.className = "filmOption";
+        if(element.poster_path!=null){
+            imgUrl = imgEndpoint + element.poster_path;
+            newDiv.style.backgroundImage = "url("+imgUrl+")";
+        }
+        else{
+            console.log("ENTRE");
+            newDiv.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png)";
+        }
+        
+        newDiv.setAttribute("filmID",element.id);
+        newDiv.setAttribute("filmTitle",element.original_title);
+        newDiv.setAttribute("releaseYear",element.release_date.slice(0,4));
+
+        newDiv.addEventListener('click', function() {
+            modal.style.display = 'flex';
+            var shownTitle = document.getElementById("filmTitle");
+            var shownYear = document.getElementById("filmYear");
+
+            shownTitle.textContent = newDiv.getAttribute("filmTitle");
+            shownYear.textContent = newDiv.getAttribute("releaseYear");
+        });
+
+        resultDiv.appendChild(newDiv)
+
+
+    });
+}
 
